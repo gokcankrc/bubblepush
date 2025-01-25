df7e0ee91631c0f7e5ea02781820afb4ae2af14a
Enes <enes@luduarts.com>
2025-01-25 15:18:47 +0300

    mouse rotation follow

--- Assets/Scripts/FollowMouse.cs
+++ Assets/Scripts/FollowMouse.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 public class FollowMouse : MonoBehaviour
 {
     public Rigidbody2D rb;
+    
+    
+    private Vector3 previousMousePosition;
+
+    public float RotationSpeed = 10f;
+
+    private void Start()
+    {
+        Vector3 screenPosition = Input.mousePosition;
+        previousMousePosition = Camera.main.ScreenToWorldPoint(screenPosition);;
+    }
 
     private void FixedUpdate()
     {
         Vector3 screenPosition = Input.mousePosition;
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-        worldPosition.z = 0;
 
+        // Rotate cursor
+        var delta = previousMousePosition - worldPosition;
+        transform.right = -Vector3.Lerp(-transform.right, delta.normalized, RotationSpeed * Time.deltaTime);
+        previousMousePosition = worldPosition;
+        
+        worldPosition.z = 0;
         rb.velocity = Vector3.zero;
         rb.MovePosition(worldPosition);
     }
