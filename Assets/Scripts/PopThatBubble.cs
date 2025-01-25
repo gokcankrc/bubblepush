54d124d4bd7143e9e2c20d4de8b2aac3c51f7a15
gokcankrc <gokcankrc@gmail.com>
2025-01-25 16:52:29 +0300

    fxı fxIX FIXEDHOT

--- Assets/Scripts/PopThatBubble.cs
+++ Assets/Scripts/PopThatBubble.cs
@@ -7,6 +7,6 @@ public class PopThatBubble : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log($"DEATH by {other.name}, {other.gameObject.layer}");
-        Destroy(gameObject);
+        Destroy(transform.parent.gameObject);
     }
 }
