d803f3aa63b23c09d22e1e5d5af5a2157f9b018c
gokcankrc <gokcankrc@gmail.com>
2025-01-25 19:36:31 +0300

    qs

--- Assets/Scripts/PopThatBubble.cs
+++ Assets/Scripts/PopThatBubble.cs
@@ -1,12 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PopThatBubble : MonoBehaviour
 {
+    public Transform rigidbodyParent;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log($"DEATH by {other.name}, {other.gameObject.layer}");
         Destroy(transform.parent.gameObject);
+
+        DestroyEffects();
     }
+
+    private void DestroyEffects()
+    {
+        GameObject[] joints = rigidbodyParent.Cast<Transform>().Select(x => x.gameObject).ToArray();
+        int count = joints.Length;
+        for (int i = 0; i < count; i++)
+        {
+            GameObject current = joints[i];
+            DeleteJoints(current);
+            ActivateSprite(current);
+        }
+
+        DeactivateMainSprite();
+        Impulse();
+    }
+
+    private void DeleteJoints(GameObject current)
+    {
+        SpringJoint2D[] oldJoints = current.GetComponents<SpringJoint2D>();
+        foreach (var joint in oldJoints)
+        {
+            DestroyImmediate(joint);
+        }
+    }
+
+    private void ActivateSprite(GameObject current)
+    {
+        current.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    private void DeactivateMainSprite() { }
+    private void Impulse() { }
 }
